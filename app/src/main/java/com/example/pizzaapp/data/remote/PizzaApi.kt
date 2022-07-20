230e76892ec0102c959439fa2c03b86b5ba9d673
CherifiMi <cherifi154949@gmail.com>
2022-07-20 09:51:23 +0100

    pizza api interface

---
+++ app/src/main/java/com/example/pizzaapp/data/remote/PizzaApi.kt
@@ -0,0 +1,10 @@
+package com.example.pizzaapp.data.remote
+
+import com.example.pizzaapp.domain.model.PizzaList
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PizzaApi {
+    @GET("/pizzas")
+    suspend fun getPizzaList():Response<PizzaList>
+}
